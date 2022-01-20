2905f8636d1b0fa4037d2e5a8a1aca8b307e1184
h-miiaah <haim.miiaah5@gmail.com>
2022-01-19 21:55:28 -0500

    else conditional

--- PascalsTriangle.js
+++ PascalsTriangle.js
@@ -13,6 +13,8 @@
         for (j = 0; j <= i; j++) {
             if (j === 0 || j === i) {
                 result[i][j] = 1;
+              } else {
+                result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
               }
           }
       }
